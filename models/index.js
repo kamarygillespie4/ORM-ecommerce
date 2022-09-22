6204250586d66ddaef21a429b62f926b02c1e7b5
Kamary <kamarygillespie@Kamarys-MBP.lan>
2022-09-22 16:42:10 -0600

    created API routes

--- models/index.js
+++ models/index.js
@@ -5,33 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+//product can only belong to one category
 Product.belongsTo(Category, {
     foreignKey: 'category_id',
 });
 
 // Categories have many Products
+// each category can have many products
 Category.hasMany(Product, {
     foreignKey: 'category_id',
 });
 
 // Products belongToMany Tags (through ProductTag)
+//products can have several tags
 Product.belongsToMany(Tag, {
     through: ProductTag,
     foreignKey: 'product_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
+//tags can go on several products
 Tag.belongsToMany(Product, {
     through: ProductTag,
     foreignKey: 'tag_id',
 });
-
-module.exports = {
-    Product,
-    Category,
-    Tag,
-    ProductTag,
-};
 
 module.exports = {
     Product,
